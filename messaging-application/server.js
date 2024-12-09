7a800d9a62d17dbe55edb5427e51970861550f09
Ale Pereira <naitwa-alexandre@gmail.com>
2024-12-09 08:14:45 -0300

    first aplication

--- messaging-application/server.js
+++ messaging-application/server.js
@@ -1,14 +1,14 @@
 const http = require('node:http');
 const url = require('node:url');
 const fs = require('node:fs');
-
+const { unescape } = require('node:querystring');
 
 const messages = ['testing'];
 const clients = [];
 
 const server = http.createServer((request, response) => {
   const urlParts = url.parse(request.url);
-  console.log(urlParts);
+  // console.log(urlParts);
 
   if (urlParts.pathname === '/') {
     fs.readFile('index.html', (err, data) => {
@@ -16,6 +16,29 @@ const server = http.createServer((request, response) => {
     });
   } else if (urlParts.pathname.substring(0, 5) == 'poll') {
 
+  } else if (urlParts.pathname.substring(0, 5) == '/msg/') {
+    const msg = unescape(urlParts.pathname.substring(5));
+    messages.push(msg);
+    while (clients.length > 0) {
+      const client = clients.pop();
+      client.end(JSON.stringify({
+        count: messages.length,
+        append: msg + '\n'
+      }));
+    }
+    response.end();
+  }
+
+  let count = urlParts.pathname.replace('/[^0-9]*/', '');
+  console.log(count);
+
+  if (messages.length > count) {
+    response.end(JSON.stringify({
+      count: messages.length,
+      append: messages.slice(count).join('\n') + '\n'
+    }));
+  } else {
+    clients.push(response);
   }
 });
 
